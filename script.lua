ecd3f443c4bf72d8d346caf115a34ab5f91a27cb
music01-pro <musicpromusic01@gmail.com>
2026-03-15 02:08:32 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,7 @@
-print("Script carregado!")
+print("Script iniciado!")
+
+local Players = game:GetService("Players")
+
+Players.PlayerAdded:Connect(function(player)
+    print(player.Name.." entrou no jogo")
+end)
